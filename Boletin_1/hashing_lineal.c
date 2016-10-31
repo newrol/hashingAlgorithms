e796911c31af0607e8e2529ddaa7a62092ebbd0b
olehdj <olehdj@gmail.com>
2016-10-31 11:40:39 +0100

    archivos txt y word
    ok

--- Boletin_1/hashing_lineal.c
+++ Boletin_1/hashing_lineal.c
@@ -1,5 +1,6 @@
 #include "hashing_lineal.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 void init(persona mitabla[]){
 
     int i;
@@ -19,16 +20,16 @@ void insert(persona mitabla[], persona reg){
     if(mitabla[p].valor != LIBRE || mitabla[p].valor != BORRADO){
 
        int i;
-       for(i=1; i< TAM; i++){
-            p2 = (p + i) % TAM;
-            if(mitabla[p2].valor == LIBRE || mitabla[p2].valor == BORRADO){
+       for(i=1; i<TAM; i++){
+            p2=(p+i) % TAM;
+            if(mitabla[p2].valor == LIBRE && mitabla[p2].valor==BORRADO){
                 mitabla[p2] = reg;
                 break;
             }
        }
 
        if(i==TAM)
-            printf("IMPOSIBLE INSERTAR VALOR %d\n", reg.valor);
+            printf("\nIMPOSIBLE INSERTAR VALOR %d\n\n", reg.valor);
 
     }
     else
@@ -38,8 +39,8 @@ void insert(persona mitabla[], persona reg){
 
 void show(persona mitabla[]){
     int i;
-    for(i=0; i < TAM; i++){
-        printf("%d|", mitabla[i].valor);
+    for(i=0; i<TAM; i++){
+        printf("%d|\n", mitabla[i].valor);
     }
     printf("\n");
 }
